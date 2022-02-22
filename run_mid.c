5c6a2137cd9abf55486024bc941d59374e59580c
Nathan <nathan.gobert2002@gmail.com>
2022-02-22 22:30:48 +0100

    Encore qq probleme sur multipipes + segf

--- run_mid.c
+++ run_mid.c
@@ -6,7 +6,7 @@
 /*   By: ngobert <ngobert@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/21 14:29:12 by ngobert           #+#    #+#             */
-/*   Updated: 2022/02/21 14:30:03 by ngobert          ###   ########.fr       */
+/*   Updated: 2022/02/22 22:28:48 by ngobert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,15 @@ void	run_mid(t_data *data, int i)
 	char	**paths;
 	char	**cmd;
 	char	*bin;
-
-	cmd = ft_split(data->argv[i], ' ');
-	paths = get_paths(data);
-	bin = get_bin(cmd[0], paths);
+	
 	data->child[i] = fork();
 	if (data->child[i] < 0)
 		ft_error("Forking error");
 	else if (data->child[i] == 0)
 	{
+		cmd = ft_split(data->argv[i], ' ');
+		paths = get_paths(data);
+		bin = get_bin(cmd[0], paths);
 		close(data->pipe[i][0]);
 		dup2(data->pipe[i - 1][0], STDIN_FILENO);
 		dup2(data->pipe[i][1], STDOUT_FILENO);
